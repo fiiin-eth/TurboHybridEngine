807c9c75a95ce7f7638bff892422cda5825afe1b
Fin Hall (s5509190) <s5509190@bournemouth.ac.uk>
2024-10-03 12:21:25 +0100

    Initial Push

---
+++ src/TurboHybridEngine/dummy.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void dummy() {
+	std::cout << "Dummy" << std::endl;
+}
---
+++ src/demo/main.cpp
@@ -0,0 +1,11 @@
+#include <myengine/dummy.h>
+
+#include <iostream>
+
+int main() {
+	std::cout << "hello world" << std::endl;
+
+	dummy();
+
+	return 0;
+}
